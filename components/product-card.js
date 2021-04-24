0349599109217620fa3ad44686d9fe4ec7011cb0
Diego F. Prado <goidesdesign@gmail.com>
2021-04-24 15:37:51 -0500

    add Files Web Components

---
+++ components/product-card.js
@@ -0,0 +1,20 @@
+class productCard extends HTMLElement{
+    constructor(){
+        super();
+        this.attachShadow({ mode: 'open'});
+    }
+
+    getTemplate(){
+
+    }
+
+    render(){
+
+    }
+
+    connectedCallback(){
+        
+    }
+}
+
+customElements.define('product-card', productCard);
